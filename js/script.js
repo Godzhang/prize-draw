4eb56b12e4a528179c117a9f75bf5343a11a8f13
godzhang <zhangqijilindaxue@126.com>
2017-10-12 00:06:35 +0800

    need to study other people's code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+	var btn = document.getElementById("btn"),
+		box = document.getElementById("box"),
+		imgs = box.getElementsByTagName('img'),
+		flag = true,
+		posArr = [];
+
+	
+
+
+
+
+
+
+}
